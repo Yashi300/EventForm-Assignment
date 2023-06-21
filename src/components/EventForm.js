ac2048373f0b5e615484d87a787748896705b77b
Yashi300 <guptayashi7103@gmail.com>
2023-06-21 19:45:22 +0530

    changes

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -8,26 +8,35 @@ function EventForm(props){
      days:[]
   })
 
-function run(event){
-    const name=event.target.name;
-      const value=event.target.value;
-      if(name!=="Days"){
-        setItem(prev=>{
-            return {
-                ...prev,
-                [name]:value
-            }
-          })
-      }else{
-        setItem(prev => {
-            return {
-              ...prev,
-              days: [...prev.days, value], // Push the value into the days array
-            };
-          });
-      }     
-console.log(item);
-}
+  function run(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    if (name !== "Days") {
+      setItem((prev) => {
+        return {
+          ...prev,
+          [name]: value,
+        };
+      });
+    } else {
+      if (item.days.includes(value)) {
+        setItem((prev) => {
+          return {
+            ...prev,
+            days: prev.days.filter((day) => day !== value),
+          };
+        });
+      } else {
+        setItem((prev) => {
+          return {
+            ...prev,
+            days: [...prev.days, value],
+          };
+        });
+      }
+    }
+  }
+  
 function submit(event){
    props.call(item);
    event.preventDefault();
@@ -36,7 +45,16 @@ function submit(event){
      sdate:"",
      edate:"",
      days:[]
-   })     
+   })  
+   const selectedOptions = document.querySelectorAll('option');
+   selectedOptions.forEach(option => option.classList.remove("alter"));   
+}
+function added(e){
+// console.log(e.target.value);
+const name = e.target.value;
+// console.log(name);
+const ele=document.getElementById(name);
+ele.classList.toggle("alter");
 }
     return <div className='event'>
         <form>
@@ -54,13 +72,13 @@ function submit(event){
             </div>
             <label htmlFor="sel">Days</label>
             <select name="Days" id='sel' onChange={run} multiple>
-                <option name="Days" value="Monday" >Monday</option>
-                <option name="days" value="Tuesday" >Tuesday</option>
-                <option name="days" value="Wednesday" >Wednesday</option>
-                <option name="days" value="Thursday" >Thursday</option>
-                <option name="days" value="Friday" >Friday</option>
-                <option name="days" value="Saturday" >Saturday</option>
-                <option name="days" value="Sunday" >Sunday</option>
+                <option name="Days" value="Monday" id='Monday' onClick={added} >Monday</option>
+                <option name="days" value="Tuesday" id='Tuesday' onClick={added}  >Tuesday</option>
+                <option name="days" value="Wednesday" id='Wednesday' onClick={added}  >Wednesday</option>
+                <option name="days" value="Thursday" id='Thursday' onClick={added}  >Thursday</option>
+                <option name="days" value="Friday" id='Friday' onClick={added}  >Friday</option>
+                <option name="days" value="Saturday" id='Saturday' onClick={added}  >Saturday</option>
+                <option name="days" value="Sunday" id='Sunday' onClick={added}  >Sunday</option>
             </select>
             <button type="submit" class="button-11" role="button" onClick={submit}>Add Event</button>
         </form>
